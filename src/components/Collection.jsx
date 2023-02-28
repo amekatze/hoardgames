f3669b1379831db39fe9b4dad1a40338db406517
amekatze <setharie@gmail.com>
2023-02-28 16:50:04 +0900

    feat: Remove game from collection

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import '../styles/collection.scss';
 
 function Collection(props) {
   const [collection, setCollection] = useState([]);
@@ -15,12 +16,47 @@ function Collection(props) {
     getCollection();
   }, [userId]);
 
+  const deleteFromCollection = async (gameId, memberId) => {
+    try {
+      const response = await fetch('http://localhost:5000/user/game', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          gameId: gameId,
+          memberId: memberId,
+        }),
+      });
+
+      const result = await response;
+      if (result.ok) {
+        setCollection(collection.filter((item) => item.game_id !== gameId));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>This is my collection</h1>
-      {collection.map((item) => {
-        return <div key={item.id}>{item.game}</div>;
-      })}
+    <div className='collection'>
+      <div className='collection-grid'>
+        {collection.map((item) => {
+          return (
+            <div className='collection-card' key={item.id}>
+              <h3>{item.game}</h3>
+              <img src={item.game_image} />
+              <button
+                onClick={() => {
+                  deleteFromCollection(item.game_id, item.member_id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
